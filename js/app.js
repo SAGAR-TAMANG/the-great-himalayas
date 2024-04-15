f8af42e8ad040c77d958a8484ebe9a360dfcefd1
Sagar Tamang <sagar.bdr0000@gmail.com>
2024-04-15 14:47:50 +0530

    fixed bugs related to particle js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,150 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   console.log("Page fully loaded");
   document.getElementById("loading-screen").style.display = "none";
   document.getElementById("main").style.display = "block";
-  checkScreenWidth()
+  checkScreenWidth();
+
+  // Particle.js starts
+
+  // particlesJS.load('particles-js', 'particles.json', function() {
+  //   console.log('particles.js loaded - callback');
+  // });
+
+  /* Otherwise just put the config content (json): */
+
+  particlesJS(
+    "particles-js",
+
+    {
+      particles: {
+        number: {
+          value: 200,
+          density: {
+            enable: true,
+            value_area: 800,
+          },
+        },
+        color: {
+          value: "#ffffff",
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000",
+          },
+          polygon: {
+            nb_sides: 5,
+          },
+          image: {
+            src: "img/github.svg",
+            width: 100,
+            height: 100,
+          },
+        },
+        opacity: {
+          value: 1,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0,
+            sync: false,
+          },
+        },
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 2,
+            size_min: 0,
+            sync: false,
+          },
+        },
+        line_linked: {
+          enable: false,
+          distance: 100,
+          color: "#ffffff",
+          opacity: 1,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 0.5,
+          direction: "bottom",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          attract: {
+            enable: false,
+            rotateX: 3000,
+            rotateY: 3000,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: true,
+            mode: ["repulse", "grab"],
+          },
+          onclick: {
+            enable: true,
+            mode: "push",
+          },
+          resize: true,
+        },
+        modes: {
+          grab: {
+            distance: 200,
+            line_linked: {
+              opacity: 1,
+            },
+          },
+          bubble: {
+            distance: 200,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3,
+          },
+          repulse: {
+            distance: 50,
+            duration: 0.4,
+          },
+          push: {
+            particles_nb: 4,
+          },
+          remove: {
+            particles_nb: 2,
+          },
+        },
+      },
+      retina_detect: true,
+      config_demo: {
+        hide_card: false,
+        background_color: "#b61924",
+        background_image: "",
+        background_position: "50% 50%",
+        background_repeat: "no-repeat",
+        background_size: "cover",
+      },
+    }
+  );
 });
 
-// Function to check screen width and show alert if it's a mobile device
 function checkScreenWidth() {
   // Get the screen width
-  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var screenWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
 
   // Check if screen width is below a certain threshold (e.g., 768 pixels)
   if (screenWidth < 768) {
-    console.log("Small Screen Detected - Please Rotate")
+    console.log("Small Screen Detected - Please Rotate");
     document.querySelector(".text h3").style.display = "block";
   }
 }
@@ -86,6 +218,15 @@ timeline
     "0.5"
   )
   .from(
+    ".text h3",
+    {
+      y: -150,
+      opacity: 0,
+      duration: 3,
+    },
+    "0.5"
+  )
+  .from(
     ".hide",
     {
       opacity: 0,
@@ -94,143 +235,12 @@ timeline
     "0.5  "
   );
 
-
-// Particle.js starts
-
-// particlesJS.load('particles-js', 'particles.json', function() {
-//   console.log('particles.js loaded - callback');
-// });
-
-/* Otherwise just put the config content (json): */
-
-particlesJS('particles-js',
-  
-  {
-    "particles": {
-      "number": {
-        "value": 200,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 1,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 3,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 2,
-          "size_min": 0,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": false,
-        "distance": 100,
-        "color": "#ffffff",
-        "opacity": 1,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 0.5,
-        "direction": "bottom",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 3000,
-          "rotateY": 3000
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": ["repulse", "grab"]
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 200,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 200,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 50,
-          "duration": 0.4
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
-
-);
-
 // Sweet Alerts
 
 const aboutButton = () => {
   Swal.fire({
     title: "Welcome!",
     html: "This is an example of a parallax website. Technologies used: Vanilla HTML CSS JS, font awesome, GSAP, Sweet Alerts, and Particle JS. <a href='https://github.com/SAGAR-TAMANG/the-great-himalayas'>More Details</a>",
-    icon: "info"
+    icon: "info",
   });
 };
